7e5e110d55f08086db2e9dad61c92f0782a94824
MohammadAyubAnsari <119043948+MohammadAyubAnsari@users.noreply.github.com>
2022-11-26 22:32:57 +0530

    Create GetElementsbyTagName.js

---
+++ GetElementsbyTagName.js
@@ -0,0 +1,28 @@
+// // GetElementByTagName
+// var li = document.getElementsByTagName('li')
+// console.log(li)
+// console.log(li[1])
+// li[1].textContent = 'Hello 2'
+// li[1].style.fontWeight = 'bold'
+// li[1].style.backgroundColor = 'yellow'
+
+// // items[2].style.backgroundColor = 'green'
+
+
+
+// // items.style.backgroundColor = '#f4f4f4' //will not work because it is a HTML collection or array
+
+// for(var i=0;i<li.length;i++){
+//     li[i].style.backgroundColor = '#f4f4f4'
+// }
+
+// var items = document.getElementsByClassName('list-group-item')
+// console.log(items)
+// items[3].style.backgroundColor = 'green'
+// items[4].style.backgroundColor = 'green'
+
+var li = document.getElementsByTagName('li')
+console.log(li)
+li[4].style.backgroundColor = 'red'
+li[3].style.backgroundColor = 'red'
+
