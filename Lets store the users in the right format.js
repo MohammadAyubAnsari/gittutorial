92c23e2349ee6faa6976d7c7c1b95d166a2a3568
MohammadAyubAnsari <mdayubansari2014@gmail.com>
2022-11-28 20:56:12 +0530

    Create Lets store the users in the right format.js

---
+++ Lets store the users in the right format.js
@@ -0,0 +1,17 @@
+function saveToLocalStorage(event){
+    event.preventDefault()
+    const name =event.target.username.value
+    const email = event.target.emailId.value
+    const phonenumber = event.target.phonenumber.value
+
+    localStorage.setItem('name',name)
+    localStorage.setItem('email',email)
+    localStorage.setItem('phonenumber',phonenumber)
+
+    const obj = {
+        name,
+        email,
+        phonenumber
+    }
+    localStorage.setItem('userDetails',JSON.stringify(obj))
+}
